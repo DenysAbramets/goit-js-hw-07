13e0538c76d9c5531f1a5449337f6905137efce0
DenysAbramets <abramets_denys@outlook.com>
2023-03-02 16:38:35 +0100

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,70 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const containerGallery = document.querySelector(".gallery");
+ const makeGallery = createGalleryImage(galleryItems);
+
+ containerGallery.innerHTML = makeGallery;
+
+function createGalleryImage(galleryItems){
+    return galleryItems.map(image => {
+        return `<div class="gallery__item">
+         <a class="gallery__link" href="${image.original}">
+           <img
+             class="gallery__image"
+             src="${image.preview}"
+             data-source="${image.original}"
+             alt="${image.description}"
+           />
+         </a>
+       </div>
+        `
+    }).join("");
+
+};
+
+    
+
+
+
+containerGallery.addEventListener("click" , onOpenModal);
+
+function onOpenModal(event){
+    if(event.target.nodeName !== 'IMG'){
+        return;
+    };
+    event.preventDefault();
+
+	 const instance = basicLightbox.create(`
+		<img width="1400" height="900"  src="${event.target.dataset.source}">
+	`);
+    instance.show();
+
+    document.addEventListener("keydown", escCloseModal )
+
+    function escCloseModal(event){
+    if(event.code == "Escape"){
+        instance.close();
+        document.removeEventListener("keydown" , escCloseModal )
+     console.log(event.code);
+    };
+}
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const containerGallery = document.querySelector(".gallery");
+ const makeGallery = createGalleryImage(galleryItems);
+
+ containerGallery.innerHTML = makeGallery;
+
+function createGalleryImage(galleryItems){
+    return galleryItems.map(image => {
+        return ` <a class="gallery__item" href="${image.original}">
+  <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+</a>
+        `
+    }).join("");
+
+};
+
+    
+
+
+
+containerGallery.addEventListener("click" , onOpenModal);
+
+function onOpenModal(event) {
+    if(event.target.nodeName !== 'IMG'){
+        return;
+    };
+    event.preventDefault();
+    const  lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+
+}
